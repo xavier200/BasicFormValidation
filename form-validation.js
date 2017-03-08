9fe5c6b78355f7ae63df14cceab4014ff6f0e67e
Unknown <xvillanueva@mail.csuchico.edu>
2017-03-07 23:19:52 -0800

    I set up the rules for a username and password

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,40 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
 
+  //Show message that there is an error with the username...
+if (userEntered.length < 6)
+    { document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+    }
+
+else { document.getElementById("usernameError").innerHTML="Good username.";
+
+document.getElementById("usernameError").classList.remove("hidden-message");
+document.getElementById("usernameError").classList.add("shown-message");
+//Turn the username items red
+document.getElementById("usernameGroup").classList.add("has-success");
+     }
+
+if (passEntered.length < 9)
   //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
+{
+  document.getElementById("passwordError").innerHTML="password needs to be longer than eight characters";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+}
+else{
+  document.getElementById("passwordError").innerHTML="good password.";
+  document.getElementById("passwordError").classList.remove("hidden-message");
+  document.getElementById("passwordError").classList.add("shown-message");
+  //Turn the password items red
+  document.getElementById("passwordGroup").classList.add("has-success");
+}
+
+
 }
